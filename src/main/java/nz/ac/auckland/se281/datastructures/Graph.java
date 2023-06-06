dca37af8c36ae2e7eff48670dd53bd4122dee625
Jay <json941@aucklanduni.ac.nz>
2023-06-06 13:42:15 +1200

    Can get correct value of roots and other properties as well

--- src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
@@ -1,8 +1,12 @@
 package nz.ac.auckland.se281.datastructures;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import org.yaml.snakeyaml.nodes.Node;
 
 /**
  * A graph that is composed of a set of verticies and edges.
@@ -24,7 +28,43 @@ public class Graph<T extends Comparable<T>> {
   public Set<T> getRoots() {
     // Check if the source and destination of edges are equal to each other.
     // If they are, then the vertex is a root.
-    throw new UnsupportedOperationException();
+    Set<T> roots = new HashSet<T>();
+    for (T vertex : verticies) {
+      for (Edge<T> edge : edges) {
+        if ((edge.getDestination() != vertex) && (edge.getSource() == vertex)) {
+          if (checkDesitination(vertex)) {
+            roots.add(vertex);
+          }
+        } else if ((edge.getDestination() == vertex) && (edge.getSource() == vertex)) {
+          if (checkDesitination(vertex)) {
+            roots.add(vertex);
+          }
+        } else if (isEquivalence()) {
+          T minValue = null;
+
+          // Iterate over the elements of the HashSet
+          Iterator<T> iterator = getEquivalenceClass(vertex).iterator();
+          while (iterator.hasNext()) {
+            T value = iterator.next();
+            if (minValue == null || value.compareTo(minValue) < 0) {
+              minValue = value;
+            }
+          }
+          roots.add(minValue);
+        }
+      }
+    }
+    return roots;
+    // throw new UnsupportedOperationException();
+  }
+
+  public boolean checkDesitination(T vertex) {
+    for (Edge<T> edge : edges) {
+      if (edge.getDestination() == vertex) {
+        return false;
+      }
+    }
+    return true;
   }
 
   public boolean isReflexive() {
@@ -71,26 +111,28 @@ public class Graph<T extends Comparable<T>> {
 
   public boolean isTransitive() {
     // Check if the graph is transitive or not.
-    int count = 0;
     for (Edge<T> edge : edges) {
       T source = edge.getSource();
       T destination = edge.getDestination();
       for (Edge<T> edge2 : edges) {
         if (edge2.getSource() == destination) {
-          for (Edge<T> edge3 : edges) {
-            if ((edge3.getSource() == source)
-                && (edge3.getDestination() == edge2.getDestination())) {
-              count++;
-            }
+          if (!containElement(source, edge2.getDestination())) {
+            return false;
           }
         }
       }
     }
-    if (count == edges.size()) {
-      return true;
+    return true;
+    // throw new UnsupportedOperationException();
+  }
+
+  public boolean containElement(T source, T destination) {
+    for (Edge<T> edge : edges) {
+      if ((edge.getSource() == source) && (edge.getDestination() == destination)) {
+        return true;
+      }
     }
     return false;
-    // throw new UnsupportedOperationException();
   }
 
   public boolean isAntiSymmetric() {
@@ -122,33 +164,210 @@ public class Graph<T extends Comparable<T>> {
       return true;
     }
     return false;
-    //throw new UnsupportedOperationException();
+    // throw new UnsupportedOperationException();
   }
 
   public Set<T> getEquivalenceClass(T vertex) {
     // Get the equivalence class of a vertex.
     Set<T> equivalenceClass = new HashSet<T>();
-    for (Edge<T> edge : edges) {
-      if (edge.getSource() == vertex) {
-        for (Edge<T> edge2 : edges) {
-          if ((edge.getDestination() == edge2.getSource()) && (edge2.getDestination() == vertex)) {
-            equivalenceClass.add(edge.getDestination());
+    if (isEquivalence()) {
+      for (Edge<T> edge : edges) {
+        T edgeSource = edge.getSource();
+        if (edgeSource.equals(vertex)) {
+          for (Edge<T> edge2 : edges) {
+            if ((edge.getDestination().equals(edge2.getSource()))
+                && (edge2.getDestination().equals(edge.getSource()))) {
+              equivalenceClass.add(edge.getDestination());
+            }
           }
         }
       }
     }
     return equivalenceClass;
-    //throw new UnsupportedOperationException();
+    // throw new UnsupportedOperationException();
   }
 
   public List<T> iterativeBreadthFirstSearch() {
-    // TODO: Task 2.
-    throw new UnsupportedOperationException();
+    // iterate through the graph using breadth first search
+    Set<T> roots = this.getRoots();
+    List<T> visited = new ArrayList<>();
+    Set<T> visitedSet = new HashSet<>();
+    CustomQueue<T> queue = new CustomQueue<>();
+
+    if (verticies.isEmpty()) {
+      return visited;
+    }
+
+    // T startVertex = roots.iterator().next();
+    // visited.add(startVertex);
+    // visitedSet.add(startVertex);
+    // queue.enqueue(startVertex);
+
+    for (T root : roots) {
+      if (!visitedSet.contains(root)) {
+        visited.add(root);
+        visitedSet.add(root);
+        queue.enqueue(root);
+      }
+
+      while (!queue.isEmpty()) {
+        T currentVertex = queue.dequeue();
+        for (Edge<T> edge : edges) {
+          if (edge.getSource().equals(currentVertex)) {
+            if (edge.getSource().equals(currentVertex)
+                && !visitedSet.contains(edge.getDestination())) {
+              
+              T nextVertex = edge.getDestination();
+              visited.add(nextVertex);
+              visitedSet.add(nextVertex);
+              queue.enqueue(nextVertex);
+            }
+          }
+        }
+      }
+    }
+
+    return visited;
+    // throw new UnsupportedOperationException();
   }
 
+  public Set<T> getVerticies(T vertex) {
+    Set<T> verticies = new TreeSet<>();
+    for (Edge<T> edge : edges) {
+      if (edge.getSource().equals(vertex)) {
+        verticies.add(edge.getDestination());
+      }
+    }
+    return verticies;
+  }
+
+  private class CustomQueue<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    boolean isEmpty() {
+      return head == null;
+    }
+
+    void enqueue(T data) {
+      Node<T> newNode = new Node<>(data);
+      if (isEmpty()) {
+        head = newNode;
+        tail = newNode;
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+    }
+
+    T dequeue() {
+      if (isEmpty()) {
+        // throw new NoSuchElementException("Queue is empty");
+      }
+      T data = head.data;
+      head = head.next;
+      if (head == null) {
+        tail = null;
+      }
+      return data;
+    }
+
+    private class Node<T> {
+      T data;
+      Node<T> next;
+
+      Node(T data) {
+        this.data = data;
+      }
+    }
+  }
+
+  class CustomStack<T> {
+    private CustomLinkedList<T> list;
+
+    public CustomStack() {
+        list = new CustomLinkedList<>();
+    }
+
+    public void push(T data) {
+        list.addFirst(data);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            //throw new NoSuchElementException("Stack is empty");
+        }
+
+        T data = list.getFirst();
+        list.removeFirst();
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
+class CustomLinkedList<T> {
+  private Node<T> head;
+
+  public void addFirst(T data) {
+      Node<T> newNode = new Node<>(data);
+      newNode.next = head;
+      head = newNode;
+  }
+
+  public T getFirst() {
+      if (isEmpty()) {
+          //throw new NoSuchElementException("List is empty");
+      }
+
+      return head.data;
+  }
+
+  public void removeFirst() {
+      if (isEmpty()) {
+          //throw new NoSuchElementException("List is empty");
+      }
+
+      head = head.next;
+  }
+
+  public boolean isEmpty() {
+      return head == null;
+  }
+
+  private static class Node<T> {
+      private T data;
+      private Node<T> next;
+
+      public Node(T data) {
+          this.data = data;
+      }
+  }
+}
+
   public List<T> iterativeDepthFirstSearch() {
-    // TODO: Task 2.
-    throw new UnsupportedOperationException();
+    List<T> visited = new ArrayList<>();
+        Set<T> visitedSet = new HashSet<>();
+        CustomStack<T> stack = new CustomStack<>();
+        stack.push(verticies.iterator().next());
+
+        while (!stack.isEmpty()) {
+            T current = stack.pop();
+            if (!visitedSet.contains(current)) {
+                visited.add(current);
+                visitedSet.add(current);
+
+                for (Edge<T> edge : edges) {
+                    if (edge.getSource().equals(current) && !visitedSet.contains(edge.getDestination())) {
+                        stack.push(edge.getDestination());
+                    }
+                }
+            }
+        }
+
+        return visited;
+    //throw new UnsupportedOperationException();
   }
 
   public List<T> recursiveBreadthFirstSearch() {
